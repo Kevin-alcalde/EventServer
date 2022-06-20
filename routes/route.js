f6ab1e41bbc66b01831f18d8817af64462e1d9f7
Kevin Alcalde <kevin.javier.alcalde@estudiantat.upc.edu>
2022-06-20 22:48:20 +0200

    commit final

--- routes/route.js
+++ routes/route.js
@@ -5,7 +5,8 @@ const fetch = require('node-fetch');
 const bcu = require('bigint-crypto-utils');
 const bigintConversion = require('bigint-conversion')
 const { RsaPublicKey } = require('../rsaPublicKey');
-const sha256 = require('crypto-js/sha256')
+const sha256 = require('crypto-js/sha256');
+const SHA256 = require('crypto-js/sha256');
 
 
 router.get("/", async (req, res) => {
@@ -23,50 +24,66 @@ router.post("/accessToEvent", async (req, res) => {
     //Handle the req tha should be the pubA and Sprivu(puba)
     //Then verify SprivU with pubU
     this.publicKeyU = await getPublicKeyU();
-    console.log("PUBLICA DE LA UNI E" + this.publicKeyU.eHex)
-    console.log("PUBLICA DE LA UNI N" + this.publicKeyU.nHex)
+    console.log("PUBLIC KEY ENVIADA DESDE LA UPC E : " + this.publicKeyU.eHex)
+    console.log("PUBLIC KEY ENVIADA DESDE LA UPC N : " + this.publicKeyU.nHex)
     console.log("IDENTIDAD SIGNATURE :" + req.body.identidadSignatureD)
     this.signature = req.body.identidadSignatureD
     this.eHexAllice = req.body.eHex
     this.nHexAllice = req.body.nHex
-    console.log ("publica allice : " + this.eHexAllice)
-    console.log ("publica allice : " + this.nHexAllice)
-    const publicKey = new RsaPublicKey( bigintConversion.hexToBigint(this.eHexAllice), bigintConversion.hexToBigint(this.nHexAllice))
-    
+    console.log("publica allice : " + this.eHexAllice)
+    console.log("publica allice : " + this.nHexAllice)
+    const allicePublicKey = new RsaPublicKey(bigintConversion.hexToBigint(this.eHexAllice), bigintConversion.hexToBigint(this.nHexAllice))
+    const publicKeyUPC = new RsaPublicKey(bigintConversion.hexToBigint(this.publicKeyU.eHex), bigintConversion.hexToBigint(this.publicKeyU.nHex))
 
-    //This is the way to 
     //Let's send a Nonce 
-    if(null !== bcu.modPow(bigintConversion.hexToBigint(req.body.identidadSignatureD), bigintConversion.hexToBigint(this.publicKeyU.eHex), 
-    bigintConversion.hexToBigint(this.publicKeyU.nHex))) {
-
-        
-    console.log(" confirmacion :" + bcu.modPow(bigintConversion.hexToBigint(req.body.identidadSignatureD), bigintConversion.hexToBigint(this.publicKeyU.eHex), 
-    bigintConversion.hexToBigint(this.publicKeyU.nHex)))
-    var nonce = "090aa2d35";
-    res.json({
-        "nonce": `${nonce}`
-    })
-    console.log("hemos entrado en el if!!")
-}
+    console.log("PORFAVOOOR  " + bigintConversion.bigintToHex(publicKeyUPC.verify(bigintConversion.hexToBigint(req.body.identidadSignatureD))))
 
 
-}
+    const prueba_verificadaPublicKey = bigintConversion.bigintToHex(publicKeyUPC.verify(bigintConversion.hexToBigint(this.signature)))
+    console.log("PORFAVOOOR en HEX " + prueba_verificadaPublicKey)
 
-)
+    let descerealizacion = {
+        e: bigintConversion.bigintToHex(this.eHexAllice),
+        n: bigintConversion.bigintToHex(this.eHexAllice)
+    }
+
+    const comprobacion = sha256(JSON.stringify(descerealizacion)).toString()
+    console.log("COMPROBARCIOOOONNG : " + comprobacion)
+
+
+    const publicUniversity = new RsaPublicKey(bigintConversion.hexToBigint(this.publicKeyU.eHex), bigintConversion.hexToBigint(this.publicKeyU.nHex))
+
+    const hashFirma = bigintConversion.bigintToHex(publicUniversity.verify(bigintConversion.hexToBigint(this.signature)))
+    console.log(" COMPROBACIOOONGGG 2: " + hashFirma)
+
+    console.log("HASH A IGUALAS " + SHA256(allicePublicKey).toString())
+
+    if (SHA256(allicePublicKey).toString() === bigintConversion.bigintToHex(publicUniversity.verify(bigintConversion.hexToBigint(this.signature))),
+        bigintConversion.hexToBigint(this.publicKeyU.nHex)) {
+        var nonce = "90aa2d35";
+        res.json({
+            "nonce": `${nonce}`
+        })
+        console.log("hemos entrado en el if!! y este es el nonce que se envia " + nonce)
+    }
+})
 
 
 router.post("/isValidAuthentication", async (req, res) => {
     console.log(req.body.nonceEnviar)
-    var resultado = bcu.modPow( bigintConversion.hexToBigint(req.body.nonceEnviar), bigintConversion.hexToBigint(this.eHexAllice), bigintConversion.hexToBigint(this.nHexAllice))
-    console.log(bigintConversion.bigintToHex(resultado) + " se parece?? " + this.nHexAllice)
-    
-    
+    var nonce = "90aa2d35"
+    const nonceFirmado = req.body.nonceEnviar
+    console.log(nonceFirmado)
 
-    
-
-
-    
-    
+    const allicePublicKey = new RsaPublicKey(bigintConversion.hexToBigint(this.eHexAllice), bigintConversion.hexToBigint(this.nHexAllice))
+    console.log("esto da la verificacion  " + bigintConversion.bigintToHex(allicePublicKey.verify(bigintConversion.hexToBigint(nonceFirmado))))
+    if (nonce = bigintConversion.bigintToHex(allicePublicKey.verify(bigintConversion.hexToBigint(nonceFirmado)))) {
+        console.log("SE ACABO??? ")
+        var veredicto = "200";
+        res.json({
+            "veredicto": `${veredicto}`
+        })
+    }
 })
 
 async function getPublicKeyU() {
@@ -77,8 +94,5 @@ async function getPublicKeyU() {
     //console.log(data);
     return data;
 }
-
-
-
 
 module.exports = router;
